bfc644fafddd59ed8a189740c9dbf7c3e9f13627
manel1954 <ea3eiz@ea3eiz.com>
2022-05-13 19:15:44 +0200

    1

--- OpenEjecutarSolofusion.sh
+++ OpenEjecutarSolofusion.sh
@@ -40,7 +40,7 @@ echo "                          ABRIENDO SOLO FUSION                            
 echo " **************************************************************************"
 sleep 2
 cd /home/pi/YSFClients/YSFGateway
-sudo xterm -geometry 90x9+1171+500 -bg black -fg white -fa 'Verdana' -fs 9x -T YSFGateway -e ./YSFGateway YSFGateway.ini & 
+sudo xterm -geometry 77x9+1275+500 -bg black -fg white -fa 'Verdana' -fs 9x -T YSFGateway -e ./YSFGateway YSFGateway.ini & 
 cd /home/pi/MMDVMHost
 #/home/pi/B108/./qt_info_solofusion & sudo ./MMDVMFUSION MMDVMFUSION.ini
 sudo ./MMDVMFUSION MMDVMFUSION.ini
