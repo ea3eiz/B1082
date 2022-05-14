801a40448f1db341288bb9434ebcb8d40fb0ff29
manel1954 <ea3eiz@ea3eiz.com>
2022-05-14 15:51:08 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-
-# path usuario
-usuario=$(awk "NR==1" /home/pi/.config/autostart/usuario)
-SCRIPTS_version=$(awk "NR==1" $usuario/.config/autostart/version)
-version=`expr substr $SCRIPTS_version 2 2`
-AUTOARRANQUE="AUTOARRANQUE"
-AUTOARRANQUE=$AUTOARRANQUE$SCRIPTS_version
-
-#Colores
-ROJO="\033[1;31m"
-VERDE="\033[1;32m"
-BLANCO="\033[1;37m"
-AMARILLO="\033[1;33m"
-CIAN="\033[1;36m"
-GRIS="\033[0m"
-MARRON="\33[38;5;138m"
-
                         git clone http://github.com/ea3eiz/B1082
                         sleep 2
                         mv B1082 B108
