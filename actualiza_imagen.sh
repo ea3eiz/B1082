ccf962723f02e64bf04ba7e052ba6622ad422bad
manel1954 <ea3eiz@ea3eiz.com>
2022-05-14 16:34:05 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -2,8 +2,7 @@
 
 
                         git clone http://github.com/ea3eiz/B1082
-                        sudo rm -R /home/pi/B108
-                        sudo mv /home/pi/B1082 /home/pi/B108
-                        sudo chmod 777 -R /home/pi/B108
-                        cd /home/pi/B108
+                        cd /home/pi/
+                        mv B1082 B108
+                        sudo chmod 777 -R B108
                         ./qt_imagen_actualizada
