158a9649d8ef502d38f7220833f77f1e6df6b7f8
manel1954 <ea3eiz@ea3eiz.com>
2022-05-14 16:13:38 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -6,7 +6,7 @@
                         read a
                         sudo rm -R /home/pi/B108
                         read a
-                        sudo mv /homepi/B1082 /home/pi/B108
+                        sudo mv /home/pi/B1082 /home/pi/B108
                         sudo chmod 777 -R /home/pi/B108
                         read a
                         cd /home/pi/B108
