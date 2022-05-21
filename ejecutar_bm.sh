914d5d4cd43666b8246132b571eba45ce0ce8393
Manel <ea3eiz@ea3eiz.com>
2022-05-21 11:23:13 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -3,7 +3,7 @@
 cd /home/pi/MMDVMHost
 echo "\33[38;5;138m"
 echo " **************************************************************************"
-echo "                          ABRIENDO BRANDMEISTER                            "
+echo "                          ABRIENDO BRANDMEISTERxxxxxxxx                            "
 echo " **************************************************************************"
 sleep 2
 #/home/pi/B108/./qt_info_bm & sudo ./MMDVMBM MMDVMBM.ini
