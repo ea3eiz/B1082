f7877d72d4f884dcc969afa18017e8e51c38533e
manel1954 <ea3eiz@ea3eiz.com>
2022-05-26 18:40:44 +0200

    1

--- OpenEjecutarSolodstar.sh
+++ OpenEjecutarSolodstar.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+
 # Cierra el Dstar del DVSwitch server
 sudo systemctl stop ircddbgatewayd
 
 # Ejecuta Solo D-STAR
 cd /home/pi/MMDVMHost
-#/home/pi/B108/./qt_info_solodstar & sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
 sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
+echo "${VERDE}"
+echo " **************************************************************************"
+echo "                          ABRIENDO SOLO DSTAR                              "
+echo " **************************************************************************"
