73c5cd1958badda3cf76d04937553312f6c6a0be
manel1954 <ea3eiz@ea3eiz.com>
2022-06-04 13:31:30 +0200

    1

--- actualiza_imagen_prueba.sh
+++ actualiza_imagen_prueba.sh
@@ -10,6 +10,18 @@
                         mv B1082 B108
                         sleep 1
                         sudo chmod 777 -R B108
+
+
+
+
+
+
                         cd /home/pi/B108
+
+sudo cp panel_configuracion.php /var/www/html/
+sudo chmod 777 /var/www/html/panel_configuracion.php
+
+
+
                         ./qt_imagen_actualizada
 
