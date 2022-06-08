7680c23c683d1142d5bd662b71b03ce165ac11c6
manel1954 <ea3eiz@ea3eiz.com>
2022-06-08 23:05:48 +0200

    1

--- ejecutar_DMR2YSF.sh
+++ ejecutar_DMR2YSF.sh
@@ -43,7 +43,7 @@ sudo cp Abrir_DMR2YSF.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_DMR2YSF.desktop
 
-cd /home/pi/DMR2YSF
+cd /opt/MMDVM_CM/DMR2YSF/
 xterm -geometry 88x11+648+420 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2YSF -e ./DMR2YSF DMR2YSF.ini &
 sleep 2
 
